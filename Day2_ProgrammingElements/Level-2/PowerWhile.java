0d7fdd9ffa864dba7f4cba26c2ff37b5c28965c6
AdityaNarayanGupta2004 <adityanarayangupta812@gmail.com>
2025-06-13 11:19:36 +0530

    Aditya Narayan Gupta Level2 of Day-2 completed

---
+++ Day2_ProgrammingElements/Level-2/PowerWhile.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PowerWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int base = sc.nextInt();
+        int exponenet = sc.nextInt();
+        int ans = 1;
+        int i=1;
+        while(i <= exponenet) {
+            ans *= base;
+            i++;
+        }
+        System.out.println(base + "to the power " + exponenet + " is = " + ans);
+
+    }
+}
