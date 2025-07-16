b53593a9733f7aca6060d53eaaabe0c43eda2871
AdityaNarayanGupta2004 <adityanarayangupta812@gmail.com>
2025-07-16 10:59:09 +0530

    AdityaNarayanGupta_Completed_Review-work

---
+++ Review/MusicPlayers/Music.java
@@ -0,0 +1,10 @@
+package MusicPlayers;
+public class Music {
+     public static void main(String[] args) {
+        MusicPlayer player = new MusicPlayer(); 
+        player.addMedia("Song A");
+        player.addMedia("Song B");
+        player.addMedia("Song C");
+        System.out.println(player.playNext("Song A"));
+    }
+}
